53dddf350750dea4d6e2f5f703cd7a3754c9d5ee
Ianbitb <ian.hurley@gmail.com>
2023-09-09 18:00:48 +0100

    Initial commit version 1.0

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+    implementation ("io.github.microutils:kotlin-logging:2.1.23")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
---
+++ src/main/kotlin/org/setu/placemark/console/main/main.kt
@@ -0,0 +1,57 @@
+package org.setu.placemark.console.main
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+    logger.info { "Launching Placemark Console App" }
+    println("Placemark Kotlin App Version 1.0")
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> addPlacemark()
+            2 -> updatePlacemark()
+            3 -> listPlacemarks()
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Placemark Console App" }
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Placemark")
+    println(" 2. Update Placemark")
+    println(" 3. List All Placemarks")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
+}
+
+fun addPlacemark(){
+    println("You Chose Add Placemark")
+}
+
+fun updatePlacemark(){
+    println("You Chose Update Placemark")
+}
+
+fun listPlacemarks(){
+    println("You Chose List All Placemarks")
+}
